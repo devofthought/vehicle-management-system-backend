a67745b811b746f6e2dbf7ac807ae5c5ab5aded5
md milon hosen <it000025@gms.com>
2024-01-07 01:54:57 +0600

    get all user

---
+++ src/app/modules/user/user.constant.ts
@@ -0,0 +1,3 @@
+export const userSearchableFields = ['userName'];
+
+export const userFilterableFields = ['searchTerm', 'role'];
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,6 +4,9 @@ import { UserService } from './user.service';
 import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
 import { Request, Response } from 'express';
+import pick from '../../../shared/pick';
+import { userFilterableFields } from './user.constant';
+import { paginationFields } from '../../../constants/pagination';
 
 // create super admin
 const createSuperAdmin = catchAsync(async (req: Request, res: Response) => {
@@ -61,9 +64,26 @@ const createHelper = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+// get all
+// get all
+const getAll = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, userFilterableFields);
+  const paginationOptions = pick(req.query, paginationFields);
+  const result = await UserService.getAll(filters, paginationOptions);
+
+  sendResponse<User[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Users retrieved successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const UserController = {
   createSuperAdmin,
   createAdmin,
   createDriver,
   createHelper,
+  getAll,
 };
